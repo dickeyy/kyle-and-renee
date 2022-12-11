86a9474b568038ebae588f265cbec55297bd5841
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2022-12-11 11:17:42 -0700

    Update GalleryPage.js

--- react-web/src/pages/GalleryPage.js
+++ react-web/src/pages/GalleryPage.js
@@ -11,7 +11,7 @@ function GalleryPage () {
                     <Text pos={'absolute'} top={5} left={5} zIndex={'hide'}>This will take a long time to load. Please be patient :)</Text>
                     <Spinner size={'xl'} pos={'absolute'} top={'50%'} left={'50%'} thickness={'thick'} zIndex={'hide'} />
                     <Text pos={'absolute'} top={'56%'} left={'48%'} fontSize={'3xl'} fontWeight={'900'} zIndex={'hide'}>Loading...</Text>
-                    <Spline aria-hidden={'true'} scene="https://prod.spline.design/GUcilEfj09AN7-Zy/scene.splinecode" />
+                    <Spline aria-hidden={'true'} scene="https://prod.spline.design/0PZuDll93QlbPtJr/scene.splinecode" />
                 </Box>
 
         </ChakraProvider> 
